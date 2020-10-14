9aa146f37e6eb548f7c52e0428378fa3f2b60c91
Crestor123 <LNinten@gmail.com>
2020-10-14 01:09:31 -0400

    Vector value object, and Character abstract

---
+++ Scriptable Objects/Character.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerState
+{   
+    WALK,
+    RUN,
+    TACKLE,
+    STUN,
+    INTERACT,
+    ABILITY
+}
+
+public class Character : ScriptableObject
+{
+    //Character data
+    public string name;
+    public Sprite icon;
+
+    //Mechanical stats
+    public float moveSpeed;
+    public VectorValue startingPosition;
+    public PlayerState currentState;
+
+    //Abstract functions for using abilities
+    public abstract void AbilityA();
+    public abstract void AbilityB();
+    public abstract void AbilityC();
+}
